a15d5e286dea6a544b274548cc2f48d355b5c2ec
Jagriti Jha <im.jagriti.jha@gmail.com>
2024-11-09 22:25:07 +0530

    implemented extension

---
+++ lib/views/home_screen.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+import 'package:whatsapp_clone/extensions.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        children: [
+          Text(''),
+          20.h,
+          Text(''),
+        ],
+      ),
+    );
+  }
+}
